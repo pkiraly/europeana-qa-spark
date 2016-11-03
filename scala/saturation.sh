0f481a5e66335a8f4f4819171fc009bef79fd9a5
pkiraly <kirunews@gmail.com>
2016-11-03 16:20:07 +0100

    Add saturation related scripts.

---
+++ scala/saturation.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+#
+# Run the top level aggregation of language features.
+#
+# Input
+#   A file take place on HDFS /join directory (such as result*-language.csv)
+#   This file should be the output of record-level language measurement
+# Output
+#   languages.csv
+
+INPUT=$1
+
+if [[ ("$#" -ne 1) || ("$INPUT" == "") ]]; then
+  echo "You should add an input file!"
+  exit 1
+fi
+
+OUTPUTFILE=saturation.csv
+
+hdfs dfs -rm -r /join/$OUTPUTFILE
+
+spark-submit \
+   --class Saturation \
+   --master local[*] \
+   target/scala-2.10/europeana-qa_2.10-1.0.jar \
+   hdfs://localhost:54310/join/ $INPUT $OUTPUTFILE
+
+echo Retrieve $OUTPUTFILE
+hdfs dfs -getmerge /join/$OUTPUTFILE $OUTPUTFILE
+
+rm .*.crc
+
+echo DONE
