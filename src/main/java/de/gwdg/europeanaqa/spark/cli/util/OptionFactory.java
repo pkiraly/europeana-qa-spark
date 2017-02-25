bea1ef03e57dd564f8bde64afa3d8b78a24be426
Peter Kiraly <peter.kiraly@gwdg.de>
2017-02-25 17:50:14 +0100

    Adding command line parameter support to Multilingual saturation.

--- src/main/java/de/gwdg/europeanaqa/spark/cli/util/OptionFactory.java
+++ src/main/java/de/gwdg/europeanaqa/spark/cli/util/OptionFactory.java
@@ -1,12 +1,13 @@
 package de.gwdg.europeanaqa.spark.cli.util;
 
+import java.io.Serializable;
 import org.apache.commons.cli.Option;
 
 /**
  *
  * @author Péter Király <peter.kiraly at gwdg.de>
  */
-public class OptionFactory {
+public class OptionFactory implements Serializable {
 
 	public static Option create(String smallOpt, String longOpt, boolean required, String desc) {
 		return create(smallOpt, longOpt, required, desc, true);
