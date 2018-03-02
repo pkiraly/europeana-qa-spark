301c3891f54856f8ab3797d552d3ec08abbb1d3d
Peter Kiraly <peter.kiraly@gwdg.de>
2018-03-02 10:09:45 +0100

    Using external API to request JSON from Mongo.

--- src/main/java/de/gwdg/europeanaqa/spark/MongoReader.java
+++ src/main/java/de/gwdg/europeanaqa/spark/MongoReader.java
@@ -49,7 +49,7 @@ public class MongoReader  implements Serializable {
 		JavaRDD<String> baseCountsRDD = rdd.map(record -> {
 			System.err.println(record);
 			System.err.println(record.toString());
-			System.err.println(record.toJson());
+			System.err.println(record.toJson(codec));
 			String id = record.get("about", String.class);
 			// System.err.println(id);
 			String jsonString = client.getRecord2(id);
