461f6fcd3e402f94653b652f485dbfe79bcd114c
Peter Kiraly <peter.kiraly@gwdg.de>
2018-05-18 20:22:39 +0200

    Improving graph analysis

--- src/main/java/de/gwdg/europeanaqa/spark/graph/VocabularyExtractor.java
+++ src/main/java/de/gwdg/europeanaqa/spark/graph/VocabularyExtractor.java
@@ -4,11 +4,15 @@ import de.gwdg.metadataqa.api.json.JsonBranch;
 import de.gwdg.metadataqa.api.model.JsonPathCache;
 import de.gwdg.metadataqa.api.model.XmlFieldInstance;
 import de.gwdg.metadataqa.api.schema.Schema;
+import net.minidev.json.JSONArray;
 
 import java.io.Serializable;
 import java.util.*;
+import java.util.logging.Logger;
 
 public class VocabularyExtractor implements Serializable {
+
+	private static final Logger logger = Logger.getLogger(VocabularyExtractor.class.getCanonicalName());
 
 	private Schema qaSchema;
 	private final Map<String, JsonBranch> entitiesPaths = new HashMap<>();
@@ -21,15 +25,30 @@ public class VocabularyExtractor implements Serializable {
 		entitiesPaths.put("timespan", qaSchema.getPathByLabel("Timespan"));
 	}
 
-	public List<Integer> getCardinality(JsonPathCache<? extends XmlFieldInstance> cache, String entityType, String entityID) {
-		String entityById = entitiesPaths.get(entityType).getJsonPath() + "[?(@['about'] == '" + entityID + "')]";
-		List<JsonBranch> childrenPaths = entitiesPaths.get(entityType).getChildren();
+	public List<Integer> getCardinality(
+		JsonPathCache<? extends XmlFieldInstance> cache,
+		String entityType,
+		String entityID)
+	{
+		String entityById = entitiesPaths.get(entityType).getJsonPath()
+			+ "[?(@['about'] == '" + entityID + "')]";
 
 		List<Integer> cardinalities = new ArrayList<>();
-		Object entity = cache.getFragment(entityById);
-		for (JsonBranch childPath : childrenPaths) {
-			List<? extends XmlFieldInstance> fieldInstances = cache.get(childPath.getJsonPath(), childPath.getJsonPath(), entity);
-			cardinalities.add(fieldInstances != null ? fieldInstances.size() : 0);
+		Object entities = cache.getFragment(entityById);
+		if (entities == null) {
+			logger.severe(String.format("Entity is null"));
+		} else {
+			if (entities instanceof JSONArray) {
+				Object entity = ((JSONArray) entities).get(0);
+				List<JsonBranch> childrenPaths = entitiesPaths.get(entityType).getChildren();
+				for (JsonBranch childPath : childrenPaths) {
+					List<? extends XmlFieldInstance> fieldInstances = cache.get(
+						childPath.getJsonPath(), childPath.getJsonPath(), entity);
+					cardinalities.add(fieldInstances != null ? fieldInstances.size() : 0);
+				}
+			} else {
+				logger.severe(String.format("Entity is not a JSONArray"));
+			}
 		}
 
 		return cardinalities;
---
+++ src/test/java/de/gwdg/europeanaqa/spark/graph/VocabularyExtractorTest.java
@@ -0,0 +1,33 @@
+package de.gwdg.europeanaqa.spark.graph;
+
+import de.gwdg.europeanaqa.spark.TestUtils;
+import de.gwdg.metadataqa.api.model.JsonPathCache;
+import de.gwdg.metadataqa.api.model.XmlFieldInstance;
+import de.gwdg.metadataqa.api.schema.EdmFullBeanSchema;
+import de.gwdg.metadataqa.api.schema.Schema;
+import org.junit.Test;
+import scala.actors.threadpool.Arrays;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class VocabularyExtractorTest {
+
+	@Test
+	public void test() throws IOException, URISyntaxException {
+		String agentId = "http://data.europeana.eu/agent/base/151990";
+		String jsonString = TestUtils.readFirstLine("edm-fullbean.json");
+		Schema schema = new EdmFullBeanSchema();
+
+		VocabularyExtractor e = new VocabularyExtractor(schema);
+		JsonPathCache<? extends XmlFieldInstance> cache = new JsonPathCache<>(jsonString);
+		List<Integer> cardinality = e.getCardinality(cache, "agent", agentId);
+		List<Integer> expected = Arrays.asList(new Integer[]{
+			1, 0, 1, 0, 0, 22, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 13, 13, 1, 0
+		});
+		assertEquals(expected, cardinality);
+	}
+}
