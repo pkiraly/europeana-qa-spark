f4de7344679ae5a51382df282b0f4a731df76423
Peter Kiraly <peter.kiraly@gwdg.de>
2018-05-18 20:27:26 +0200

    Improving graph analysis

--- src/test/java/de/gwdg/europeanaqa/spark/graph/VocabularyExtractorTest.java
+++ src/test/java/de/gwdg/europeanaqa/spark/graph/VocabularyExtractorTest.java
@@ -9,6 +9,7 @@ import org.junit.Test;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
