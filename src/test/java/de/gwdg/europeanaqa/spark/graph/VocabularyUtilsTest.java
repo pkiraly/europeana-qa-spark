3be009379cafdfbf63745f7787d2c28e0751a7f8
Peter Kiraly <peter.kiraly@gwdg.de>
2018-05-22 09:52:36 +0200

    Improving vocabulary extraction.

--- src/test/java/de/gwdg/europeanaqa/spark/graph/VocabularyUtilsTest.java
+++ src/test/java/de/gwdg/europeanaqa/spark/graph/VocabularyUtilsTest.java
@@ -15,6 +15,18 @@ public class VocabularyUtilsTest {
 		assertEquals("dddd", VocabularyUtils.extractPLD("4881/SP.1"));
 		assertEquals("2.ddd", VocabularyUtils.extractPLD("2,294/SP.1"));
 		assertEquals("1.ddd", VocabularyUtils.extractPLD("1,167/SP.1"));
+		assertEquals("oai:nid.pl:2", VocabularyUtils.extractPLD("oai:nid.pl:2:117/SP.1"));
+		assertEquals("adlib.project", VocabularyUtils.extractPLD("adlib.project.20013695/SP.1"));
+		assertEquals("280_place", VocabularyUtils.extractPLD("#POR_280_place01"));
+		assertEquals("280_place", VocabularyUtils.extractPLD("#ES_280_place1"));
+		assertEquals("geo:xy", VocabularyUtils.extractPLD("geo:51.5844832546,4.77953553181"));
+		assertEquals("UAEMAT", VocabularyUtils.extractPLD("UAEMAT-1/SP.1"));
+		assertEquals("ort_herstellung_uuid", VocabularyUtils.extractPLD("ort_herstellung_uuid=41eb6b27-4607-0044-eea0-09a5f5bd28fc"));
+		assertEquals("48.2d#latitude", VocabularyUtils.extractPLD("48.200525#latitude"));
+		assertEquals("#LIT_placeN", VocabularyUtils.extractPLD("#LIT_place1"));
+		assertEquals("#placeN", VocabularyUtils.extractPLD("#place1"));
+		assertEquals("#place-abc", VocabularyUtils.extractPLD("#place-paris-barcelona"));
+		assertEquals("SPANISH", VocabularyUtils.extractPLD("Abaceria Central, mercat de l'"));
 	}
 
 	@Test
