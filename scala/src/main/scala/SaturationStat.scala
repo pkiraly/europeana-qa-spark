0baa05cdb0f29ecde38f4f8310fa13796bd1fe5e
Peter Kiraly <peter.kiraly@gwdg.de>
2018-08-13 17:57:51 +0200

    Adding initial saturation statistics.

--- scala/src/main/scala/SaturationStat.scala
+++ scala/src/main/scala/SaturationStat.scala
@@ -2,7 +2,7 @@ import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
 
-object SaturationTest {
+object SaturationStat {
   def main(args: Array[String]): Unit = {
 
     val conf = new SparkConf().setAppName("SaturationStat")
