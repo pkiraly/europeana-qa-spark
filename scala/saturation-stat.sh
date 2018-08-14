e48f59f59f8cdfd8376fc766e803663acfcb0105
pkiraly <kirunews@gmail.com>
2018-08-14 10:08:15 +0200

    input/output parameters for saturation statistics.

--- scala/saturation-stat.sh
+++ scala/saturation-stat.sh
@@ -10,15 +10,19 @@
 
 INPUT=$1
 
-
+INPUTFILE=result29-multilingual-saturation-light.csv
 OUTPUTFILE=result29-multilingual-saturation-light-statistics
+HDFS_DIR=hdfs://localhost:54310/join
 
 hdfs dfs -rm -r /join/$OUTPUTFILE
 
 spark-submit \
+   --driver-memory 3g \
    --class SaturationStat \
    --master local[6] \
-   target/scala-2.10/europeana-qa_2.10-1.0.jar
+   target/scala-2.11/europeana-qa_2.11-1.0.jar \
+   $HDFS_DIR/$INPUTFILE
+   $HDFS_DIR/$OUTPUTFILE
 
 echo Retrieve $OUTPUTFILE
 hdfs dfs -getmerge /join/$OUTPUTFILE $OUTPUTFILE
