acda43b5de4d4839266b61d0b6cbe3d908f1b0a7
Peter Kiraly <peter.kiraly@gwdg.de>
2018-08-16 22:31:15 +0200

    Median - 2nd version - for the whole

--- scala/src/main/scala/SaturationWithHistogramForAll.scala
+++ scala/src/main/scala/SaturationWithHistogramForAll.scala
@@ -201,7 +201,8 @@ object SaturationWithHistogramForAll {
           .over(Window.partitionBy("group").orderBy($"label")))
         .withColumn("start", (col("end") - col("count")))
 
-      zerosRow = zerosRow :+ histogram.filter($"label" === 0).select("count").first().getInt(0)
+      var zeros = histogram.filter($"label" === 0).select("count").first().getInt(0)
+      zerosRow = zerosRow :+ (zeros * 100 / total)
 
       if (isImpair) {
         l = (total / 2)
