eda9d783baf090a58ea22030b1ed93eeb97ca177
Peter Kiraly <peter.kiraly@gwdg.de>
2019-04-09 21:31:17 +0200

    Improve language count.

--- src/main/java/de/gwdg/europeanaqa/spark/LanguageCount.java
+++ src/main/java/de/gwdg/europeanaqa/spark/LanguageCount.java
@@ -20,54 +20,54 @@ import org.apache.spark.api.java.function.Function;
  */
 public class LanguageCount {
 
-	private static final Logger logger = Logger.getLogger(LanguageCount.class.getCanonicalName());
-	private static final boolean withLabel = false;
-	private static final boolean compressed = true;
+  private static final Logger logger = Logger.getLogger(LanguageCount.class.getCanonicalName());
+  private static final boolean withLabel = false;
+  private static final boolean compressed = true;
 
-	public static void main(String[] args) throws FileNotFoundException, ParseException {
+  public static void main(String[] args) throws FileNotFoundException, ParseException {
 
-		if (args.length < 1) {
-			System.err.println("Please provide a full path to the input files");
-			System.exit(0);
-		}
-		if (args.length < 2) {
-			System.err.println("Please provide a full path to the output file");
-			System.exit(0);
-		}
+    if (args.length < 1) {
+      System.err.println("Please provide a full path to the input files");
+      System.exit(0);
+    }
+    if (args.length < 2) {
+      System.err.println("Please provide a full path to the output file");
+      System.exit(0);
+    }
 
-		Parameters parameters = new Parameters(args);
+    Parameters parameters = new Parameters(args);
 
-		String inputFileName = parameters.getInputFileName();
-		logger.info("Input file is " + inputFileName);
-		SparkConf conf = new SparkConf().setAppName("LanguageCount");
-		JavaSparkContext context = new JavaSparkContext(conf);
+    String inputFileName = parameters.getInputFileName();
+    logger.info("Input file is " + inputFileName);
+    SparkConf conf = new SparkConf().setAppName("LanguageCount");
+    JavaSparkContext context = new JavaSparkContext(conf);
 
-		final EdmCalculatorFacade calculator = CalculatorFacadeFactory.createLanguageCalculator(parameters);
-		calculator.setExtendedFieldExtraction(parameters.getExtendedFieldExtraction());
+    final EdmCalculatorFacade calculator = CalculatorFacadeFactory.createLanguageCalculator(parameters);
+    calculator.setExtendedFieldExtraction(parameters.getExtendedFieldExtraction());
 
-		JavaRDD<String> inputFile = context.textFile(inputFileName);
-		Function<String, String> baseCounts = new Function<String, String>() {
-			@Override
-			public String call(String jsonString) throws Exception {
-				try {
-					return calculator.measure(jsonString);
-				} catch (InvalidJsonException e) {
-					logger.severe(String.format("Invalid JSON in %s. Error message: %s.",
-							jsonString, e.getLocalizedMessage()));
-				}
-				return "";
-			}
-		};
+    JavaRDD<String> inputFile = context.textFile(inputFileName);
+    Function<String, String> baseCounts = new Function<String, String>() {
+      @Override
+      public String call(String jsonString) throws Exception {
+        try {
+          return calculator.measure(jsonString);
+        } catch (InvalidJsonException e) {
+          logger.severe(String.format("Invalid JSON in %s. Error message: %s.",
+              jsonString, e.getLocalizedMessage()));
+        }
+        return "";
+      }
+    };
 
-		JavaRDD<String> baseCountsRDD = inputFile.map(baseCounts);
-		baseCountsRDD.saveAsTextFile(parameters.getOutputFileName());
+    JavaRDD<String> baseCountsRDD = inputFile.map(baseCounts);
+    baseCountsRDD.saveAsTextFile(parameters.getOutputFileName());
 
-		try {
-			calculator.saveDataProviders(parameters.getDataProvidersFile());
-			calculator.saveDatasets(parameters.getDatasetsFile());
-		} catch (UnsupportedEncodingException ex) {
-			logger.severe(ex.getLocalizedMessage());
-		}
-	}
+    try {
+      calculator.saveDataProviders(parameters.getDataProvidersFile());
+      calculator.saveDatasets(parameters.getDatasetsFile());
+    } catch (UnsupportedEncodingException ex) {
+      logger.severe(ex.getLocalizedMessage());
+    }
+  }
 
 }
