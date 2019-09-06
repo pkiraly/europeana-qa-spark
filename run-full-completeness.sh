4e71d8226af3a1156c130af4538ba4180635eede
Peter Kiraly <peter.kiraly@gwdg.de>
2019-09-06 15:17:38 +0200

    Improve directories.

--- run-full-completeness.sh
+++ run-full-completeness.sh
@@ -46,8 +46,10 @@ if [[ ! -d limbo ]]; then
   mkdir limbo
 fi
 
+$LOG_DIR=$(readlink -e logs)
+
 time=$(date +"%T")
-LOG_FILE=logs/run-all-proxy-based-completeness.log
+LOG_FILE=$LOG_DIR/run-all-proxy-based-completeness.log
 echo "$time> Running proxy based completeness. Check log file: ${LOG_FILE}"
 echo "./run-all-proxy-based-completeness --output-file ${CSV} --extended-field-extraction --version ${VERSION} > ${LOG_FILE}"
 ./run-all-proxy-based-completeness --output-file ${CSV} --extended-field-extraction --version ${VERSION} &> ${LOG_FILE}
@@ -59,24 +61,24 @@ echo "$time> Collecting new abbreviation entries (if any)"
 cd scala
 
 time=$(date +"%T")
-LOG_FILE=proxy-based-completeness-to-parquet.log
-echo "$time> create parquet file. Check log file: scala/${LOG_FILE}"
+LOG_FILE=$LOG_DIR/proxy-based-completeness-to-parquet.log
+echo "$time> create parquet file. Check log file: ${LOG_FILE}"
 ./proxy-based-completeness-to-parquet.sh ../${CSV} &> ${LOG_FILE}
 
 time=$(date +"%T")
-LOG_FILE=proxy-based-completeness-all.log
-echo "$time> run completeness analysis. Check log file: scala/${LOG_FILE}"
+LOG_FILE=$LOG_DIR/proxy-based-completeness-all.log
+echo "$time> run completeness analysis. Check log file: ${LOG_FILE}"
 ./proxy-based-completeness-all.sh ../${PARQUET} keep_dirs &> ${LOG_FILE}
 
 time=$(date +"%T")
 cd ../scripts/
-LOG_FILE=split-completeness.log
-echo "$time> split results. Check log file: scripts/${LOG_FILE}"
-./split-completeness.sh ${OUTPUT_DIR}
+LOG_FILE=$LOG_DIR/split-completeness.log
+echo "$time> split results. Check log file: ${LOG_FILE}"
+./split-completeness.sh ${OUTPUT_DIR} &> ${LOG_FILE}
 
 time=$(date +"%T")
-LOG_FILE=create-intersection.log
-echo "$time> create intersection. Check log file: scripts/${LOG_FILE}"
+LOG_FILE=$LOG_DIR/create-intersection.log
+echo "$time> create intersection. Check log file: ${LOG_FILE}"
 php create-intersection.php ${OUTPUT_DIR} &> ${LOG_FILE}
 
 if [ ! -d ${WEB_DATA_DIR} ]; then
@@ -86,8 +88,8 @@ fi
 cp proxy-based-intersections.json ${WEB_DATA_DIR}
 
 time=$(date +"%T")
-LOG_FILE=clear-abbreviations.log
-echo "$time> Clear abbreviations. Check log file: scripts/${LOG_FILE}"
+LOG_FILE=$LOG_DIR/clear-abbreviations.log
+echo "$time> Clear abbreviations. Check log file: ${LOG_FILE}"
 php clear-abbreviations.php ${WEB_DATA_DIR} ${OUTPUT_DIR} &> ${LOG_FILE}
 
 duration=$SECONDS
