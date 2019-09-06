57092433615d9a63396c3d84f5ae11623914a804
Peter Kiraly <peter.kiraly@gwdg.de>
2019-09-06 15:25:53 +0200

    Improve directories.

--- run-full-completeness.sh
+++ run-full-completeness.sh
@@ -48,10 +48,9 @@ fi
 
 LOG_DIR=$(readlink -e logs)
 echo $LOG_DIR
-exit;
 
 time=$(date +"%T")
-LOG_FILE=$LOG_DIR/run-all-proxy-based-completeness.log
+LOG_FILE=${LOG_DIR}/run-all-proxy-based-completeness.log
 echo "$time> Running proxy based completeness. Check log file: ${LOG_FILE}"
 echo "./run-all-proxy-based-completeness --output-file ${CSV} --extended-field-extraction --version ${VERSION} > ${LOG_FILE}"
 ./run-all-proxy-based-completeness --output-file ${CSV} --extended-field-extraction --version ${VERSION} &> ${LOG_FILE}
@@ -63,23 +62,23 @@ echo "$time> Collecting new abbreviation entries (if any)"
 cd scala
 
 time=$(date +"%T")
-LOG_FILE=$LOG_DIR/proxy-based-completeness-to-parquet.log
+LOG_FILE=${LOG_DIR}/proxy-based-completeness-to-parquet.log
 echo "$time> create parquet file. Check log file: ${LOG_FILE}"
 ./proxy-based-completeness-to-parquet.sh ../${CSV} &> ${LOG_FILE}
 
 time=$(date +"%T")
-LOG_FILE=$LOG_DIR/proxy-based-completeness-all.log
+LOG_FILE=${LOG_DIR}/proxy-based-completeness-all.log
 echo "$time> run completeness analysis. Check log file: ${LOG_FILE}"
 ./proxy-based-completeness-all.sh ../${PARQUET} keep_dirs &> ${LOG_FILE}
 
 time=$(date +"%T")
 cd ../scripts/
-LOG_FILE=$LOG_DIR/split-completeness.log
+LOG_FILE=${LOG_DIR}/split-completeness.log
 echo "$time> split results. Check log file: ${LOG_FILE}"
 ./split-completeness.sh ${OUTPUT_DIR} &> ${LOG_FILE}
 
 time=$(date +"%T")
-LOG_FILE=$LOG_DIR/create-intersection.log
+LOG_FILE=${LOG_DIR}/create-intersection.log
 echo "$time> create intersection. Check log file: ${LOG_FILE}"
 php create-intersection.php ${OUTPUT_DIR} &> ${LOG_FILE}
 
@@ -90,7 +89,7 @@ fi
 cp proxy-based-intersections.json ${WEB_DATA_DIR}
 
 time=$(date +"%T")
-LOG_FILE=$LOG_DIR/clear-abbreviations.log
+LOG_FILE=${LOG_DIR}/clear-abbreviations.log
 echo "$time> Clear abbreviations. Check log file: ${LOG_FILE}"
 php clear-abbreviations.php ${WEB_DATA_DIR} ${OUTPUT_DIR} &> ${LOG_FILE}
 
